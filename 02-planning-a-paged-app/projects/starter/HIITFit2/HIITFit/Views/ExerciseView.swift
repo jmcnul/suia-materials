a523ac1e6f73e360c38dfc9e0354314978fce8dd
jmcnul <jmcnul@gmail.com>
2024-01-15 15:32:15 -0800

    added Exerceise and Welcome Views

--- 02-planning-a-paged-app/projects/starter/HIITFit2/HIITFit/Views/ExerciseView.swift
+++ 02-planning-a-paged-app/projects/starter/HIITFit2/HIITFit/Views/ExerciseView.swift
@@ -32,13 +32,12 @@
 
 import SwiftUI
 
-struct NewView: View {
+struct ExerciseView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)   
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    NewView()
+    ExerciseView()
 }
