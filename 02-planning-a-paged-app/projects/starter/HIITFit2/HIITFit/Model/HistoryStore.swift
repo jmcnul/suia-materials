b1d410d08a62212e31287f6610c53103610ed5b0
jmcnul <jmcnul@gmail.com>
2024-01-19 13:34:13 -0800

    refactored history view

--- 02-planning-a-paged-app/projects/starter/HIITFit2/HIITFit/Model/HistoryStore.swift
+++ 02-planning-a-paged-app/projects/starter/HIITFit2/HIITFit/Model/HistoryStore.swift
@@ -14,22 +14,3 @@ struct HistoryStore {
         #endif
     }
 }
-extension HistoryStore{
-    mutating func createDevData() {
-        exerciseDays = [
-        ExerciseDay(
-            date: Date().addingTimeInterval(-86400),
-                    exercises: [
-                        Exercise.exercises[0].exerciseName,
-                        Exercise.exercises[1].exerciseName,
-                        Exercise.exercises[2].exerciseName
-                    ]),
-        ExerciseDay(
-            date: Date().addingTimeInterval(-86400 * 2),
-                   exercises:[
-                    Exercise.exercises[1].exerciseName,
-                    Exercise.exercises[0].exerciseName
-                   ])
-        ]
-    }
-}
